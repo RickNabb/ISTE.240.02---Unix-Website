5fc99fb68e9f3e08d31a1ccabbeda9645b720a1f
nrabb <nrabb@rochester.rr.com>
2013-09-26 09:40:18 -0400

    Added front page, responsive icon navigation

---
+++ Mockup/header.php
@@ -0,0 +1,9 @@
+echo 
+'<div class="row-fluid span12">
+	<ul class="nav-pills">
+		<li><a href="#">Nav 1</a></li>
+		<li><a href="#">Nav 2</a></li>
+		<li><a href="#">Nav 3</a></li>
+	</ul>
+</div>
+<p>This is another test</p>';
