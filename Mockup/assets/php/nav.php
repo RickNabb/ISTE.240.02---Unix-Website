6ec6fc0328b8a332f9d4dabb146b1490a8595693
tablespoon1 <bennyval@gmail.com>
2013-11-11 10:29:42 -0500

    Added some dummy files, moved nav to php
    nav php is commented out, aded dummy files based off of about.html. Got
    some of the links to work (removed placeholder JS)

---
+++ Mockup/assets/php/nav.php
@@ -0,0 +1,16 @@
+<div class="clear"></div>
+    <div id="header" class="row-fluid">
+	    <div class="offset1 span5" style="width: 320px;">
+            <ul class="nav-pills">
+                <li class=""><a href="javascript:void(0);"><div class="nav_icon" id="nav_home"></div>Home</a></li>
+                <li><a href="javascript:void(0);"><div class="nav_icon" id="nav_learn_unix"></div>Learn Unix</a></li>        
+            </ul>
+	    </div>
+        <div class="span1"><p class="title">Unix</p></div>
+        <div class="span5 offset1" style="width: 340px;">
+            <ul class="nav-pills">
+                <li><a href="javascript:void(0);"><div class="nav_icon" id="nav_about_unix"></div>About Unix</a></li>
+                <li><a href="javascript:void(0);"><div class="nav_icon" id="nav_about_us"></div>About Us</a></li>        
+            </ul>
+	    </div>
+    </div>
