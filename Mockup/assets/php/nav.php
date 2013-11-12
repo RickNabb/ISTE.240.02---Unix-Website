40ff817ed882e310bad244f991485c0d46ee6fa8
root <tjb2597@rit.edu>
2013-11-12 11:30:48 -0500

    Swapped order of nav stuff, slightly modified left local nav on tut page

--- Mockup/assets/php/nav.php
+++ Mockup/assets/php/nav.php
@@ -1,16 +1,18 @@
 <div class="clear"></div>
-    <div id="header" class="row-fluid">
-	    <div class="offset1 span5" style="width: 320px;">
-            <ul class="nav-pills">
-                <li class=""><a href="javascript:void(0);"><div class="nav_icon" id="nav_home"></div>Home</a></li>
-                <li><a href="javascript:void(0);"><div class="nav_icon" id="nav_learn_unix"></div>Learn Unix</a></li>        
-            </ul>
-	    </div>
-        <div class="span1"><p class="title">Unix</p></div>
-        <div class="span5 offset1" style="width: 340px;">
-            <ul class="nav-pills">
-                <li><a href="javascript:void(0);"><div class="nav_icon" id="nav_about_unix"></div>About Unix</a></li>
-                <li><a href="javascript:void(0);"><div class="nav_icon" id="nav_about_us"></div>About Us</a></li>        
-            </ul>
-	    </div>
+<div id="header">
+    <div class="left_nav">
+        <ul>
+            <li><a href="index.html"><div class="nav_icon" id="nav_home"></div>Home</a></li>
+            <li><a href="learn.html"><div class="nav_icon" id="nav_learn_unix"></div>Learn Unix</a></li>        
+        </ul>
     </div>
+    <div class="center_nav"><p class="title" style="margin: 0; padding: 0;">About Us</p></div>
+    <div class="right_nav">
+        <ul> 
+            <li><a href="history.html"><div class="nav_icon" id="nav_about_unix"></div>About Unix</a></li>
+            <li><a href="#" class="curPage"><div class="nav_icon" id="nav_about_us"></div>About Us</a></li>
+        </ul>     
+    </div>
+</div>
+
+<div class="separator"></div>
