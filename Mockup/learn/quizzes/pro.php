afb254a8da495c5dee1ce5ccf9b376c6da82bcc4
Jpk4147 <Jpk4147@g.rit.edu>
2013-12-09 22:43:37 -0500

    Update pro.php

--- Mockup/learn/quizzes/pro.php
+++ Mockup/learn/quizzes/pro.php
@@ -20,7 +20,7 @@
     
 
     <div id="content" class="row-fluid span10">
-    
+    <!--links for tutorials-->
         <div class="pull-left well" >
             <h2>Tutorials</h2>
             <ol>
@@ -32,6 +32,7 @@
             <div class="separator"></div>
             <h2 style="margin-top: 50px;">Quizzes</h2>
             <ol>
+            <!--links for quizzes-->
                 <li><a href="beginner.html">Beginner</a></li>
                 <li><a href="intermediate.html">Intermediate</a></li>
                 <li><a href="advanced.html">Advanced</a></li>
@@ -40,73 +41,74 @@
         </div>
 	
         <div class="span8">
+        <!--Pro quiz questions-->
             <h2>Pro Unix Quiz <small>Test your knowledge</small></h2>
 
-            <h4 class="clear50">What series of commands would search the entire file directory underneath and including the current folder for files with the extension *.txt, print them to a file called ‘allTextFiles.dat’, and then mail the contents of that file to the email address ‘test@example.com’ with a subject of “All Text Files”?</h4>
+            <h4 class="clear50">What series of commands would search the entire file directory underneath and including the current folder for files with the extension *.txt, print them to a file called ï¿½allTextFiles.datï¿½, and then mail the contents of that file to the email address ï¿½test@example.comï¿½ with a subject of ï¿½All Text Filesï¿½?</h4>
             <div class="radio">
                 <label>
                     <input type="radio" name="optionsRadios" id="Radio1" value="option1">
-                       ls -R *.txt > allTextFiles.dat | mail -s ‘All Text Files’ test@example.com < allTestFiles.dat
+                       ls -R *.txt > allTextFiles.dat | mail -s ï¿½All Text Filesï¿½ test@example.com < allTestFiles.dat
 
                 </label>
             </div>
             <div class="radio">
                 <label>
                     <input type="radio" name="optionsRadios" id="Radio2" value="option2">
-                        ls -R | grep .txt > allTextFiles.dat; mail -c ‘All Text Files’ test@example.com > allTestFiles.dat
+                        ls -R | grep .txt > allTextFiles.dat; mail -c ï¿½All Text Filesï¿½ test@example.com > allTestFiles.dat
 
                 </label>
             </div>
             <div class="radio">
                 <label>
                     <input type="radio" name="optionsRadios" id="Radio3" value="option3">
-                        ls -R | grep .txt < allTextFiles.dat | mail -s ‘All Text Files’ test@example.com > allTestFiles.dat
+                        ls -R | grep .txt < allTextFiles.dat | mail -s ï¿½All Text Filesï¿½ test@example.com > allTestFiles.dat
                 </label>
             </div>
             <div class="radio">
                 <label>
                     <input type="radio" name="optionsRadios" id="Radio4" class="correct" value="option4">
-                        ls -R | grep .txt > allTextFiles.dat; mail -s ‘All Text Files’ test@example.com < allTestFiles.dat
+                        ls -R | grep .txt > allTextFiles.dat; mail -s ï¿½All Text Filesï¿½ test@example.com < allTestFiles.dat
                 </label>
             </div>
 
 
 
-            <h4 class="clear25">What is the proper command to set the command ‘doSomething’ to list the *.txt files of a directory and all subsequent subdirectories?
+            <h4 class="clear25">What is the proper command to set the command ï¿½doSomethingï¿½ to list the *.txt files of a directory and all subsequent subdirectories?
             </h4>
             <div class="radio">
                 <label>
                     <input type="radio" name="optionsRadios2" id="Radio1" value="option1">
-                        set doSomething = ’dir -a -.txt’
+                        set doSomething = ï¿½dir -a -.txtï¿½
 
                 </label>
             </div>
             <div class="radio">
                 <label>
                     <input type="radio" name="optionsRadios2" id="Radio2" class="correct" value="option2">
-                        alias doSomething = ‘ls -R | grep .txt’
+                        alias doSomething = ï¿½ls -R | grep .txtï¿½
 
                 </label>
             </div>
             <div class="radio">
                 <label>
                     <input type="radio" name="optionsRadios2" id="Radio3" value="option3">
-                        set doSomething = ‘ls -R *.txt’
+                        set doSomething = ï¿½ls -R *.txtï¿½
                 </label>
             </div>
             <div class="radio">
                 <label>
                     <input type="radio" name="optionsRadios2" id="Radio4" value="option4">
-                        alias doSomething = ‘dir -a | grep .txt’
+                        alias doSomething = ï¿½dir -a | grep .txtï¿½
                 </label>
             </div>
 
 
-            <h4 class="clear25">How would I see if my friend, whose username is “buddy1234” is logged into the same Unix system I am by using a search command?</h4>
+            <h4 class="clear25">How would I see if my friend, whose username is ï¿½buddy1234ï¿½ is logged into the same Unix system I am by using a search command?</h4>
             <div class="radio">
                 <label>
                     <input type="radio" name="optionsRadios3" id="Radio1" value="option1">
-                        ls -user ‘buddy1234’
+                        ls -user ï¿½buddy1234ï¿½
                 </label>
             </div>
             <div class="radio">
@@ -125,7 +127,7 @@
             <div class="radio">
                 <label>
                     <input type="radio" name="optionsRadios3" id="Radio4" value="option4">
-                        net -user ‘buddy1234’
+                        net -user ï¿½buddy1234ï¿½
                 </label>
             </div>
             <button type="submit" class="clear25 btn btn-primary">
