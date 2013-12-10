ea842801a2e4467d561951fe118b4a740c95526d
tablespoon1 <bennyval@gmail.com>
2013-12-10 11:45:16 -0500

    Updated validateQuizzes
    Accounted for addiotnal input tag which messed up the numQuestions
    calculation

--- Mockup/assets/scripts/validateQuizzes.js
+++ Mockup/assets/scripts/validateQuizzes.js
@@ -2,12 +2,13 @@ function validateQuestions() {
 		// get all radio inputs
 		var answers = document.getElementsByTagName("input");
 		var answersRight = 0;
-		var numQuestions = answers.length/4;
+		// subtract 1 from answers.length to account for additional input tag in embedded unix terminal
+		var numQuestions = (answers.length-1)/4;
 		
 		// loop through all input besides submit button (last input element)
 		for (var i=0; i<answers.length; i++){
 			// reset values
-			answers[i].parentNode.parentNode.style.backgroundColor="#FAFAFA";
+			answers[i].parentNode.parentNode.style.backgroundColor="transparent";
 			answers[i].parentNode.parentNode.style.border ="none";
 			answers[i].parentNode.parentNode.style.boxShadow ="none";
 			if (answers[i].checked){
